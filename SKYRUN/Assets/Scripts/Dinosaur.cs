0c3fe019173ca9e0b80877ee71688de8e096e8c4
Denzel18rb <74129875rb@gmail.com>
2026-03-01 21:31:05 -0500

    Clean Unity gitignore~

--- SKYRUN/Assets/Scripts/Dinosaur.cs
+++ SKYRUN/Assets/Scripts/Dinosaur.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Dinosaur : MonoBehaviour
 {
-    [SerializeField] private float upForce;
+    [SerializeField] private float upForce = 5f;
+    [SerializeField] private int maxJumps = 1;
+
+    private int jumpCount;
     private Rigidbody2D dinoRb;
+
     void Start()
     {
         dinoRb = GetComponent<Rigidbody2D>();
     }
 
-    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
         {
-            dinoRb.AddForce(Vector2.up * upForce);
+            dinoRb.AddForce(Vector2.up * upForce, ForceMode2D.Impulse);
+            jumpCount++;
         }
-        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
     }
 }
---
+++ SKYRUN/Assets/Scripts/FloorSegment.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FloorSegment : MonoBehaviour
+{
+    public float speed = 5f;
+    public float destroyX = -30f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (transform.position.x <= destroyX)
+        {
+            Destroy(gameObject); // 🔥 elimina Grid + Tilemap completo
+        }
+    }
+}
